4bc524afbc2f92e460d3bb4a8e0064c4b767df67
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-09-18 12:00:04 +0300

    Fixed some of the css for the auth forms

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/auth/auth.css')}}" 
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}" 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
